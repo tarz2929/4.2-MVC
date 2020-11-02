953642c256082eb63efe63e37e1239a8210d7417
James Grieve <jamesonrgrieve@gmail.com>
2020-11-02 09:15:20 -0700

    Initial commit;

---
+++ FirstASP/FirstASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
