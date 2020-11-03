f7882a7af3b05f5356a76e7cacbf1bd6bcf9e018
James Grieve <jamesonrgrieve@gmail.com>
2020-11-03 10:18:32 -0700

    EF model creation;

--- FirstASP/Models/Person.cs
+++ FirstASP/Models/Person.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 
 namespace FirstASP.Models
 {
-    public class Person
+    // In-class practice: Fill out the manufacturer class to function as a basic model. Foreign key stuff not needed.
+
+    // Challenge: Implement the foreign key.
+    [Table("person")]
+    class Person
     {
+        public Person()
+        {
+            EMailAddresses = new HashSet<EMailAddress>();
+        }
+
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
         public string FirstName { get; set; }
+
+        [Column(TypeName = "varchar(30)")]
         public string LastName { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        [InverseProperty(nameof(Models.EMailAddress.Person))]
+        public virtual ICollection<EMailAddress> EMailAddresses { get; set; }
     }
 }
---
+++ FirstASP/Models/PersonContext.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework_4Point2.Models
+{
+    class DealershipContext : DbContext
+    {
+
+        public virtual DbSet<EMailAddress> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connection =
+                    "server=localhost;" +
+                    "port=3306;" +
+                    "user=root;" +
+                    "database=codefirst_4point2;";
+
+                string version = "10.4.14-MariaDB";
+
+                optionsBuilder.UseMySql(connection, x => x.ServerVersion(version));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .HasCharSet("utf8mb4")
+                .HasCollation("utf8mb4_general_ci");
+
+                entity.HasData(
+                    new Person()
+                    {
+                        ID = -1,
+                        Name = "Ford"
+                    },
+                    new Person()
+                    {
+                        ID = -2,
+                        Name = "Chevrolet"
+                    },
+                    new Person()
+                    {
+                        ID = -3,
+                        Name = "Dodge"
+                    }
+                );
+            });
+            modelBuilder.Entity<EMailAddress>(entity =>
+            {
+                string keyName = "FK_" + nameof(EMailAddress) +
+                    "_" + nameof(Person);
+
+                // These SHOULD be set automatically. If you want to play around with it by removing these and verify this version of EF works that way, feel free. 
+                entity.Property(e => e.Model)
+                .HasCharSet("utf8mb4")
+                .HasCollation("utf8mb4_general_ci");
+
+                entity.Property(e => e.Colour)
+                .HasCharSet("utf8mb4")
+                .HasCollation("utf8mb4_general_ci");
+
+                entity.HasIndex(e => e.PersonID)
+                .HasName(keyName);
+
+                entity.HasOne(thisEntity => thisEntity.Person)
+                .WithMany(parent => parent.EMailAddresses)
+                .HasForeignKey(thisEntity => thisEntity.PersonID)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName(keyName);
+
+                // In-class Practice 2:
+                // Add seed data for 5 vehicles similarly to the manufacturers above.
+                entity.HasData(
+                    new EMailAddress()
+                    {
+                        ID = -1,
+                        PersonID = -1,
+                        Colour = "Blue",
+                        Model = "Fusion",
+                        ModelYear = 2010
+                    },
+                    new EMailAddress()
+                    {
+                        ID = -2,
+                        PersonID = -1,
+                        Colour = "Black",
+                        Model = "Escape",
+                        ModelYear = 2014
+                    },
+                    new EMailAddress()
+                    {
+                        ID = -3,
+                        PersonID = -2,
+                        Colour = "Red",
+                        Model = "Cruze",
+                        ModelYear = 2012
+                    },
+                    new EMailAddress()
+                    {
+                        ID = -4,
+                        PersonID = -3,
+                        Colour = "Black",
+                        Model = "Ram",
+                        ModelYear = 2018
+                    },
+                    new EMailAddress()
+                    {
+                        ID = -5,
+                        PersonID = -3,
+                        Colour = "Blue",
+                        Model = "Charger",
+                        ModelYear = 2016
+                    }
+                    );
+
+            });
+        }
+    }
+}
