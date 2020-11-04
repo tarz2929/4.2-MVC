b7d8c1ffe5162c1333db26aaa366b46c8b44d6f3
James Grieve <jamesonrgrieve@gmail.com>
2020-11-04 13:01:53 -0700

    In-class practice 6 solution;

---
+++ ProductInformation/ProductInformation/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductInformation.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
