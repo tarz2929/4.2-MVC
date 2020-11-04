2509b6a9e1f2c4fc3750d088b8dcb3167ebf2531
James Grieve <jamesonrgrieve@gmail.com>
2020-11-04 11:05:01 -0700

    Fixed erroneous namespace segment; In-class practice 4;

--- ProductInformation/ProductInformation/Models/ProductInfoContext.cs
+++ ProductInformation/ProductInformation/Models/ProductInfoContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace FirstASP.Models
+namespace ProductInformation.Models
 {
     public class ProductInfoContext : DbContext
     {
