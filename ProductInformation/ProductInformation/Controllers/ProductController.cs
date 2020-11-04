ff22ed399c3aa9a1c42f5ef61f7c051e7ee64f31
James Grieve <jamesonrgrieve@gmail.com>
2020-11-04 13:21:27 -0700

    In-class practice 7 solution;

--- ProductInformation/ProductInformation/Controllers/ProductController.cs
+++ ProductInformation/ProductInformation/Controllers/ProductController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProductInformation.Models;
 
 namespace ProductInformation.Controllers
 {
     public class ProductController : Controller
     {
+        // Actions (View Endpoints):
         public IActionResult Index()
         {
             return RedirectToAction("List");
@@ -20,7 +22,21 @@ namespace ProductInformation.Controllers
 
         public IActionResult List()
         {
+            ViewBag.Products = GetProducts();
             return View();
+        }
+
+
+
+        // Data Methods:
+        public List<Product> GetProducts()
+        {
+            List<Product> results;
+            using (ProductInfoContext context = new ProductInfoContext())
+            {
+                results = context.Products.ToList();
+            }
+            return results;
         }
     }
 }
